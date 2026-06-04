c2d9d6a19c240d11e01cbb4e786aa69fbbf0a25e
thiagomktgourmet <thiagoferreiracamargos@gmail.com>
2026-06-04 16:24:24 -0300

    Create robots.js

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://blog.marketinggourmet.com.br/sitemap.xml',
+  }
+}
